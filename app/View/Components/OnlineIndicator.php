c60c6db03d879e1909a48198b3111eefe565148e
NsXpress <157027154+NsXpress@users.noreply.github.com>
2024-02-07 21:45:31 +0100

    Added missing component app file

---
+++ app/View/Components/OnlineIndicator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use App\Models\User;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class OnlineIndicator extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public User $user,
+    ) {}
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.online-indicator');
+    }
+}
